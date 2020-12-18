44e5a132e3e5def289bbad16973a303c0d53a000
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2020-12-18 17:51:43 +0900

    バリデーションの記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 before_action :basic_auth 
+before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -7,5 +8,10 @@ before_action :basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    added_attrs = [ :nickname, :email, :encrypted_password, :family_name, :first_name, :family_name_kana, :first_name_kana :birth_date ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # 正規表現
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+
+  # バリデーション
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :encrypted_password, presence: true, confirmation: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :family_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :family_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :birth_date, presence: true
+
+  # パスワードの2回入力を必須にする
+  validates :email_confirmation, presence: true
+
+  # アソシエーション
   # has_many :items
   # has_many :phachase_records
 end
