1ae9a7869653e33d43659286977d455e94242ba5
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2020-12-21 14:33:45 +0900

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana,
                                              :birth_date])
+    
   end
 end
