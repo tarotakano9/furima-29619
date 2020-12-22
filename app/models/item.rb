9f7a25838a1b7a987c4326736a429b5e942d59a1
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2020-12-22 15:30:09 +0900

    ActiveHashを用いてカテゴリーモデルを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,22 @@
-class Item < ApplicationRecord
+class Item < ApplicationRecord  
+  # ActiveHashで生成したcategoryモデルと紐づける
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
   # ActiveStorageとの紐付け
   has_one_attached :image
   
   # アソシエーション
   belongs_to :user
   # has_one :purchase_record
+
+  # バリデーションを設定する
+  with_options presence: true do
+    validetes :name
+    validetes :price
+    validetes :description
+  end
+
+  # カテゴリーが「--」の時は保存できないようにする
+  validetes :category_id, numericality: { other_than: 1 }
 end
--- db/migrate/20201221090452_create_items.rb
+++ db/migrate/20201221090452_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
-    create_table :items do |t|
-      t.string :name, null: false
-      t.integer :price, null: false
-      t.text :description, null: false
-      t.integer :category_id, null: false
-      t.integer :condition_id, null: false
-      t.integer :delivery_fee_id, null: false
-      t.integer :prefecture_id, null: false
-      t.integer :days_to_ship_id, null: false
-      t.integer :user, foreign_key: true
+    create_table  :items do |t|
+      t.string    :name               , null: false
+      t.integer   :price              , null: false
+      t.text      :description        , null: false
+      t.integer   :category_id        , null: false
+      t.integer   :condition_id       , null: false
+      t.integer   :delivery_fee_id    , null: false
+      t.integer   :prefecture_id      , null: false
+      t.integer   :days_to_ship_id    , null: false
+      t.integer   :user               , foreign_key: true
       t.timestamps
     end
   end
