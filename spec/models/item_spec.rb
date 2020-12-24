e9b0cbea12fcdf3dc013c60e44cb27a39916d797
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2020-12-24 14:40:04 +0900

    テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,25 +32,50 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
+      it 'カテゴリーが空だと失敗' do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
       it 'カテゴリーが「--」だと失敗' do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Category must be other than 1')
+      end
+      it '商品の状態が空だと失敗' do
+        @item.condition_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it '商品の状態が「--」だと失敗' do
         @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Condition must be other than 1')
       end
+      it '発送料の負担が空だと失敗' do
+        @item.delivery_fee_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+      end
       it '発送料の負担が「--」だと失敗' do
         @item.delivery_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Delivery fee must be other than 1')
       end
+      it '発送元の地域が空だと失敗' do
+        @item.prefecture_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
       it '発送元の地域が「--」だと失敗' do
         @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
+      end
+      it '発送までの日数が空だと失敗' do
+        @item.days_to_ship_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship can't be blank")
       end
       it '発送までの日数が「--」だと失敗' do
         @item.days_to_ship_id = 1
@@ -84,6 +109,11 @@ RSpec.describe Item, type: :model do
       end
       it '商品の価格が半角英語だと失敗' do
         @item.price = 'thousand'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '商品の価格が半角英数金剛だと失敗' do
+        @item.price = '500yen'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
