fbc636b68a6b6ebbc0d4eb49515341072ceff160
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2020-12-31 11:44:42 +0900

    エラーメッセージ が出るようになった

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.save
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path(item.id)
     else
       render :edit
