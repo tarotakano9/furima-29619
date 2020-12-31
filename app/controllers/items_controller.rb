9ee0b499f58a1a2589aba33ad58ce3fcae96a9d8
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2020-12-31 11:33:55 +0900

    編集完了or 失敗後の画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,11 @@ class ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
+    if item.save
+      redirect_to item_path(item.id)
+    else
+      render :edit
+    end
   end
 
   private
