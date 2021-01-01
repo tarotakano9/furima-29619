46c41725aae821e2de3139c9d6113a14e80e1dc9
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2021-01-01 18:56:45 +0900

    デストロイアクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    
+    item = Item.find(params[:id])
+    item.destroy
+    if item.destroy
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
