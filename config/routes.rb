669eb341198cc65d6d062d22429c595afb3552fd
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2021-01-02 20:28:54 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
-  resources :items
-  resources :orders, only: [:index, :create]
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 end
