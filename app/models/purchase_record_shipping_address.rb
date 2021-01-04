a0c57f51a419422e2aab26257a5a2580cbb1b1d4
tarotakano9 <73771714+tarotakano9@users.noreply.github.com>
2021-01-04 17:57:45 +0900

    電話番号のバリデーションを修正

--- app/models/purchase_record_shipping_address.rb
+++ app/models/purchase_record_shipping_address.rb
@@ -10,8 +10,8 @@ class PurchaseRecordShippingAddress
 
   # shipping_addressに関するバリデーション
   # 正規表現
-  VALID_POSTAL_CODE_REGIX = /\A\d{3}[-]\d{4}\z/
-  VALID_PHONE_NUMBER_REGIX = /\A\d{3}[-]\d{4}[-]\d{4}\z/
+  VALID_POSTAL_CODE_REGIX = /\A\d{3}[-]\d{4}\z/ # 3桁 - 4桁の半角数字
+  VALID_PHONE_NUMBER_REGIX = /\A\d{10,11}\z/ # 10~11桁の数字
 
   # バリデーション
   with_options presence: true do
@@ -24,12 +24,14 @@ class PurchaseRecordShippingAddress
   end
 
   def save
+    # ユーザーの情報を保存して変数userに格納
     user = User.create(nickname: nickname, email: email, password: password, family_name: family_name, first_name: first_name, family_name_kana: family_name_kana, first_name_kana: first_name_kana, birth_date: birth_date)
+    # 商品の情報を保存して変数itemに格納
     item = Item.create(name: name, price: price, description: description, category_id: category_id, delivery_fee_id: delivery_fee_id, prefecture_id: prefecture_id, days_to_ship_id: days_to_ship_id, user_id, user.id)
 
-    # purchase_recordを保存
+    # 購入記録を保存して変数purchase_recordに格納
     purchase_record = PurchaseRecord.create(user_id: user.id, item_id: item.id)
     # shipping_addressを保存
-    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address_line: address_line, building: building, phone_number: phone_number, purchase_record_id, purchase_record.id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address_line: address_line, building: building, phone_number: phone_number, purchase_record_id: purchase_record.id)
   end
 end
